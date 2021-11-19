1982693f2ba26a67eb2db4b51ca545000d19a2ed
Kulinich Ekaterina <kulinich.ekaterina.andreevna@gmail.com>
2021-11-19 18:41:44 +0300

    wip html, css, js

--- dist/assets/js/app.js
+++ dist/assets/js/app.js
@@ -1,1 +1,43 @@
-console.log("file 1"),console.log("file 2");
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (function() { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/assets/js/file2.js":
+/*!********************************!*\
+  !*** ./src/assets/js/file2.js ***!
+  \********************************/
+/***/ (function() {
+
+eval("console.log('file 2');\r\n\n\n//# sourceURL=webpack://kulinich/./src/assets/js/file2.js?");
+
+/***/ }),
+
+/***/ "./src/assets/js/modal.js":
+/*!********************************!*\
+  !*** ./src/assets/js/modal.js ***!
+  \********************************/
+/***/ (function() {
+
+eval("const modalBtn = document.querySelectorAll(['data-modal']);\r\n\r\nmodalBtn.forEach(item => {\r\n  item.addEventListener('click', event => {\r\n    let $this = event.currentTarget;\r\n    let modalId = $this.getAttribute('data-modal');\r\n\r\n    console.log(modalId);\r\n  });\r\n});\n\n//# sourceURL=webpack://kulinich/./src/assets/js/modal.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	__webpack_modules__["./src/assets/js/file2.js"]();
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/assets/js/modal.js"]();
+/******/ 	
+/******/ })()
+;
---
+++ src/assets/js/modal.js
@@ -0,0 +1,10 @@
+const modalBtn = document.querySelectorAll(['data-modal']);
+
+modalBtn.forEach(item => {
+  item.addEventListener('click', event => {
+    let $this = event.currentTarget;
+    let modalId = $this.getAttribute('data-modal');
+
+    console.log(modalId);
+  });
+});
